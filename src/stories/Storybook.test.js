2acb1ddeb22e6fd43d0e3fb6c1a005247546bbd4
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2021-02-24 21:27:08 +1300

    Change Storybook tests with puppeteer; add snapshots

---
+++ src/stories/Storybook.test.js
@@ -0,0 +1,14 @@
+import path from 'path';
+
+import initStoryshots from '@storybook/addon-storyshots';
+
+// the required import from the @storybook/addon-storyshots-puppeteer addon
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
+
+initStoryshots({
+    // your own configuration
+    test: imageSnapshot({
+        // invoke the function above here
+        storybookUrl: 'http://localhost:6006'
+    })
+});
