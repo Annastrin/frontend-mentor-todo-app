9d0426ae8ccc637eb58ed505614a279f5702dafe
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2021-03-30 22:29:17 +1300

    Change custom.d.ts

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,6 +4,6 @@ declare module '*.svg?inline' {
 }
 
 declare module "*.svg" {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  const content: any;
   export default content;
 }
