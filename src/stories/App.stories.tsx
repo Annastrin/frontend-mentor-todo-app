df13eb10f892bee7e7a3fec587ecfd0a9a62cf97
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2021-04-18 22:21:39 +1200

    Replace storybook provider; use mock store

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -1,15 +1,44 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
-import Provider from "./Provider";
-import { store } from "../redux/store";
-import { App } from "../components/App";
+import { Provider } from "react-redux";
+import { Story } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { App } from "../components/App/App";
+import * as TaskListStories from "./TaskList.stories";
 
-const withProvider = (StoryComponent: () => React.ReactElement) => (
-  <Provider store={store}>
-    <StoryComponent />
-  </Provider>
-);
+const store: any = {
+  getState: () => {
+    return {
+      tasks: TaskListStories.Default.args?.tasks,
+      activeFilter: TaskListStories.Default.args?.activeFilter,
+    };
+  },
+  subscribe: () => 0,
+  dispatch: (reduxAction: any) => {
+    const handler = action(reduxAction.type);
+    handler(reduxAction);
+  },
+};
 
-storiesOf("App", module)
-  .addDecorator(withProvider)
-  .add("default", () => <App />);
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "App",
+  component: App,
+  decorators: [
+    (StoryComponent: () => React.ReactElement) => (
+      <Provider store={store}>
+        <StoryComponent />
+      </Provider>
+    ),
+  ],
+  argTypes: {
+    onToggleTaskState: { action: "clicked" },
+    onRemoveTask: { action: "removed" },
+    onShowAll: { action: "show all tasks" },
+    onShowActive: { action: "show active tasks" },
+    onShowCompleted: { action: "show completed tasks" },
+    onClearCompleted: { action: "clear completed tasks" },
+  },
+};
+
+const Template: Story = (args) => <App {...args} />;
+export const Default = Template.bind({});
