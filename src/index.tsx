42c2d811842f697b4de3fd55488ca64fac2facd7
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2021-08-17 16:00:46 +1200

    Add redux-persist to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import { App } from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import rootReducer from "./reducers";
 
-export const store = configureStore({ reducer: rootReducer });
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({ reducer: persistedReducer });
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
